911053e54ffd34683b33765cca87bb91184c36e1
Alexeimtz <105665026+Alexeimtz@users.noreply.github.com>
2024-05-06 14:13:52 -0600

    actividad 2
    se subió la actividad 2 Básicos de R

---
+++ retoAnalitica/Codigo/basicos_R.R
@@ -0,0 +1,52 @@
+
+# Ejercicio_1 -------------------------------------------------------------
+
+num1 <- 3.3
+num2 <- 5.8
+resul <- num1 + num2
+sprintf("La suma de los dos numeros es:%f ", resul)
+
+
+# Ejercicio_2 -------------------------------------------------------------
+
+lado <- 4.6
+area <- lado ^ 2
+sprintf("El area de un cuadrado es:%f ", area)
+
+
+# Ejercicio_3 -------------------------------------------------------------
+
+base <- 8.9
+altura <-2.6
+areat <- (base * altura) / 2
+sprintf("El area de un triangulo es:%f ", areat)
+
+# Ejercicio_4 -------------------------------------------------------------
+
+radio <- 8
+areac <- pi * radio ^ 2
+sprintf("El area de un circulo es:%f ", areac)
+
+
+# Ejercicio_5 -------------------------------------------------------------
+
+radio <- 11.5
+altura <- 5.4
+volumen <- 1/3 * pi * (radio ^ 2) * altura
+sprintf("El volumen de un cono es:%f ", volumen)
+
+# Ejercicio_6 -------------------------------------------------------------
+
+peso <- 20
+dolar <- 50.7
+cambio <- peso * dolar
+sprintf("Tus dolares en cambio de divisa a peso: $%f ", cambio)
+
+
+# Ejercicio_7 -------------------------------------------------------------
+
+farenheit <- 234.8
+celcius <- (5/9) * (farenheit - 32)
+sprintf("la conversion de la temperatura de farenheit a celcius es :%f ", celcius)
+
+
